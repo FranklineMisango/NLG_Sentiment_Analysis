5a6125569a66ec5e80e6a1a710dfa3c0848c1c87
Frankline Misango Oyolo <95913228+FranklineMisango@users.noreply.github.com>
2024-06-11 08:36:46 +0000

    added the openAI pipeline

--- openai_trainer.py
+++ openai_trainer.py
@@ -1,23 +1,24 @@
+from openai import OpenAI
+import streamlit as st
+client = OpenAI(api_key=st.secrets["OpenAIAPI"])
 
-import openai
-
-# Set your OpenAI API key
-openai.api_key = st.secrets["OpenAIAPI"]
+# Set the OpenAI API key
 
 # Define a function to perform sentiment analysis
 def sentiment_analysis(text):
     # Use the OpenAI API to get the sentiment of the input text
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=f"Analyze the sentiment of the following text:\n\n{text}\n\nSentiment:",
-        max_tokens=1,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
+    response = client.completions.create(
+    prompt = f"Analyze the sentiment of the following text:\n\n{text}\n\nSentiment:",
+    n = 1,
+    stop = None,
+    model = "gpt-3.5-turbo",
+    max_tokens = 512,
+    top_p =  1,
+    temperature = 0.5,
+    frequency_penalty = 0,
+    presence_penalty = 0)
 
     # Extract the sentiment from the API response
     sentiment = response.choices[0].text.strip().lower()
 
     return sentiment
-
