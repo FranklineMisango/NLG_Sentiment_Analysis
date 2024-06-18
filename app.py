185b6bc87bec8c61150605bfc712fc4d7c41f83d
misango <franklinemisango@gmail.com>
2024-06-18 23:03:25 +0800

    work on the speed ; updated README with meta credits

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from summaries import summarize_all_articles
 st.set_page_config(layout="wide")
 
 def main():
-    st.title("Frankline & Co. LP. Self-rostering Ticker sentiment Analysis")
-    st.success("Pipelining done to suit OpenAILLM completions")
+    st.title("Frankline & Co. Self-rostering Ticker sentiment Analysis")
+    st.success("Pipelining done with OpenAI")
 
     ticker = st.text_input("Enter the stock/crypto ticker you want to monitor:")
 
@@ -17,7 +17,7 @@ def main():
     predict_button = st.button("Predict")
 
     if predict_button:
-        st.header(f"Analysis for {ticker}, wait a few seconds....")
+        st.header(f"BART Summarization for {ticker}, wait a few moments....")
         excluded_list = ['maps', 'policies', 'preferences', 'support', 'accounts']
         raw_urls = {ticker: search_for_stock_news_urls(ticker, source_choice)}
         cleaned_urls = {ticker: strip_unwanted_urls(raw_urls[ticker], excluded_list)}
@@ -25,24 +25,11 @@ def main():
         final_summaries = {ticker: summarize_all_articles(articles[ticker])}
         st.write(final_summaries)
 
-        st.header("BERT Sentiment Analysis for the summaries below...")
-        negative_summaries_count = 0
-
+        st.header("ChatGPT Did the sentiment Analysis for the summaries below.....")
         for summary in final_summaries[ticker].values():
             st.write(summary)
             final_scores = sentiment_analysis(summary)
-            st.write(final_scores)
-            negative_count = sum(1 for x in final_scores if x['label'] == 'NEGATIVE')
-            if negative_count > 0:
-                negative_summaries_count += 1
-
-        st.write(f"Total negative summaries post-sentiment analysis for {ticker}: {negative_summaries_count}")
-        if negative_summaries_count > 5:
-            st.warning(f"The model finds that stock {ticker} is not doing well currently. We recommend that you don't buy for short holding.")
-        elif negative_summaries_count == 5:
-            st.info(f"The model generates a neutral view on stock {ticker}, further research is needed: Bloomberg / Expedia refinement.")
-        else:
-            st.success(f"The model finds that stock {ticker} is good to buy for the short term. Contact our brokers to buy.")
+            st.success(final_scores)
 
 if __name__ == '__main__':
     main()
