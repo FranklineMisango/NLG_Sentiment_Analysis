1faa28ac421ca187c44c2d70de05b74c37454059
Frankline Misango Oyolo <95913228+FranklineMisango@users.noreply.github.com>
2024-08-31 14:02:09 +0800

    uncomment to test facebook bart

--- summarize_pipeline.py
+++ summarize_pipeline.py
@@ -1,3 +1,4 @@
 # Use a pipeline as a high-level helper
 from transformers import pipeline
-summarize_pipe = pipeline("summarization", model="facebook/bart-large-cnn")
+#summarize_pipe = pipeline("summarization", model="facebook/bart-large-cnn")
+summarize_pipe = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6", device=-1)
