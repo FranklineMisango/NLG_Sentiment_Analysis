93ffbd21cbe7e499d40a773af275ada741087170
Frankline Misango Oyolo <95913228+FranklineMisango@users.noreply.github.com>
2024-08-31 14:09:08 +0800

    pt; suggestions

--- summarize_pipeline.py
+++ summarize_pipeline.py
@@ -1,4 +1,5 @@
 # Use a pipeline as a high-level helper
 from transformers import pipeline
 #summarize_pipe = pipeline("summarization", model="facebook/bart-large-cnn")
-summarize_pipe = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6", device=-1)
+# Load the model using PyTorch weights
+summarize_pipe = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6", framework="pt")
