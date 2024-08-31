d45b4bcf6d3e0286f7cf05f22fd0a9eb7e774cbd
Frankline Misango Oyolo <95913228+FranklineMisango@users.noreply.github.com>
2024-08-31 14:10:53 +0800

    class inferrations

--- summarize_pipeline.py
+++ summarize_pipeline.py
@@ -1,5 +1,13 @@
 # Use a pipeline as a high-level helper
-from transformers import pipeline
 #summarize_pipe = pipeline("summarization", model="facebook/bart-large-cnn")
 # Load the model using PyTorch weights
-summarize_pipe = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6", framework="pt")
+from transformers import pipeline, AutoTokenizer, AutoModelForSeq2SeqLM
+
+# Load the tokenizer
+tokenizer = AutoTokenizer.from_pretrained("sshleifer/distilbart-cnn-12-6")
+
+# Load the model using PyTorch weights
+model = AutoModelForSeq2SeqLM.from_pretrained("sshleifer/distilbart-cnn-12-6")
+
+# Create the pipeline
+summarize_pipe = pipeline("summarization", model=model, tokenizer=tokenizer)
