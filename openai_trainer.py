d9dfd665760146ac6227bab5834635ab1fae42e5
FranklineMisango <franklinemisango@gmail.com>
2024-09-26 18:48:15 +0800

    changes for EC2

--- openai_trainer.py
+++ openai_trainer.py
@@ -1,8 +1,12 @@
 import openai
 import streamlit as st
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
 
 # Set the OpenAI API key
-openai.api_key = st.secrets["OpenAIAPI"]
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 # Define a function to perform sentiment analysis
 def sentiment_analysis(text):
--- summarize_pipeline.py
+++ summarize_pipeline.py
@@ -1,13 +1,8 @@
 # Use a pipeline as a high-level helper
-#summarize_pipe = pipeline("summarization", model="facebook/bart-large-cnn")
+
+
 # Load the model using PyTorch weights
 from transformers import pipeline, AutoTokenizer, AutoModelForSeq2SeqLM
 
-# Load the tokenizer
-tokenizer = AutoTokenizer.from_pretrained("sshleifer/distilbart-cnn-12-6")
-
-# Load the model using PyTorch weights
-model = AutoModelForSeq2SeqLM.from_pretrained("sshleifer/distilbart-cnn-12-6")
-
-# Create the pipeline
-summarize_pipe = pipeline("summarization", model=model, tokenizer=tokenizer)
+# Create the pipeline with GPU support
+summarize_pipe = pipeline("summarization", model="facebook/bart-large-cnn")
